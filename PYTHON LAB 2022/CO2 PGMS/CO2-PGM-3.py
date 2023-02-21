77de99e45291bbe7591fa52613c949c1ddb45e5c
anjalipa2001 <120706917+anjalipa2001@users.noreply.github.com>
2023-02-21 19:29:53 +0530

    Delete PYTHON LAB 2022/CO2 PGMS directory

--- PYTHON LAB 2022/CO2 PGMS/CO2-PGM-3.py
+++
@@ -1,4 +0,0 @@
-#Find the sum of all items in a list
-list=[2,3,4,5,8]
-s=sum(list)
-print("sum=",s)
--- PYTHON LAB 2022/CO2 PGMS/CO2-PGM-4.py
+++
@@ -1,8 +0,0 @@
-#Generate a list of four digit numbers in a given range with all their digits even and the number is a perfect square. 
-from math import sqrt
-list=[]
-n=int(input("Enter a limit:"))
-p=0
-for i in range(1000,n):
-    if sqrt(i)==int(sqrt(i)) and i%2==0:
-        print(i)
